e935f33121549f00915656886a954f999a4e4a37
igarasy <igarasshi.lucass@gmail.com>
2022-01-04 20:32:12 -0300

    Utilizando o método require para import

---
+++ node/pastaA/pastaB/acessarModulo.js
@@ -0,0 +1,8 @@
+/* Para sair de uma pasta usamos o <../> 
+Sempre colocar os nomes dos arquivos respeitando maiúsculas e minúsculas
+*/
+
+const moduloA = require('../../moduloA')
+console.log(moduloA.ola)
+
+
