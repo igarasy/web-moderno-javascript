c11a3ba49290f41f055fa1abcf569428e10e0b7e
igarasy <igarasshi.lucass@gmail.com>
2022-01-04 21:24:28 -0300

    Entendendo o arquivo package.json

---
+++ node/funcionarios/entendendoPackageJson.js
@@ -0,0 +1,10 @@
+/*  Podemos criar um arquivo package.json através do <npm init> no terminal, selecionando algumas informações 
+
+Também podemos criar através do <npm init -y>, dessa forma ele irá responder sim para todas perguntas feitas
+
+O package.json serve também para descrever todas denpendencias (bibliotecas) que o seu projeto precisa para rodar
+
+Quando instalamos com <npm i --save> seguido do nome da dependencia, ele irá adicionar no package.json o nome dela 
+
+Quando instalamos com <npm i --save-dev> seguido do nome da dependencia
+*/
