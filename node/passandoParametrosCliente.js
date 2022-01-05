09390d69c7865042181a49c09336d908b248e803
igarasy <igarasshi.lucass@gmail.com>
2022-01-04 22:37:41 -0300

    Passando parametrôs entre módulos

---
+++ node/passandoParametrosCliente.js
@@ -0,0 +1,5 @@
+/*Como o require retorna uma função, podemos passar os parâmetros logo em seguida */
+
+const saudacoes = require('./passandoParametros')('Ana', 'Lucas', 'João')
+
+console.log(saudacoes)
