507d47a9691494bc858c5f6586e2599fe6f33a0b
igarasy <igarasshi.lucass@gmail.com>
2022-01-06 19:58:31 -0300

    Utilizando o Tagged Template

---
+++ esnext/taggedTemplate.js
@@ -0,0 +1,15 @@
+// tagged templates - processa o template dentro de uma função
+
+// Pegando um valor e transformando em monetário
+function real(partes, ...valores) {
+  const resultado = []
+  valores.forEach((valor, indice) => {
+    valor = isNaN(valor) ? valor : `R$ ${valor.toFixed(2)}`
+    resultado.push(partes[indice], valor)
+  })
+  return resultado.join('')
+}
+
+const preco = 29.99
+const precoParcela = 11
+console.log(real`1x de ${preco} ou 3x de ${precoParcela}.`)
