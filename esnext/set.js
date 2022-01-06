81ad84a2b491e6f74a5f5c343a2c55d718ccdf99
igarasy <igarasshi.lucass@gmail.com>
2022-01-06 20:23:16 -0300

    Entendendo o objeto Set

---
+++ esnext/set.js
@@ -0,0 +1,21 @@
+/* Não aceita repetição e não é indexada(não podemos acessar por índice) 
+
+Caso tenha um elemento que se repete ele irá ignorar
+*/
+
+const times = new Set()
+times.add('Vasco')
+times.add('São Paulo').add('Palmeiras').add('Corinthians')
+times.add('Flamengo')
+times.add('Vasco')
+
+console.log(times)
+console.log(times.size)
+console.log(times.has('vasco'))
+console.log(times.has('Vasco'))
+times.delete('Flamengo')
+console.log(times.has('Flamengo'))
+
+const nomes = ['Raquel', 'Lucas', 'Julia', 'Lucas']
+const nomesSet = new Set(nomes)
+console.log(nomesSet)
